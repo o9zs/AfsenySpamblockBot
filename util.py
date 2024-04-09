25e5e346aeb9a22c2e579a74e99e4e514f92496a
o9zs <arsenyojike@gmail.com>
2024-04-09 21:34:29 +0300

    Add documentation

--- util.py
+++ util.py
@@ -1,4 +1,16 @@
 def data_dict(data: str) -> dict:
+	"""
+	Turns a CallbackQuery's data into a dictionary.
+
+	### Parameters
+	data : str
+		Should be in format: `"name=key1:value1,key2:value2"`
+
+	### Returns
+	dict
+		Dictionary generated from data:  `{key1: value1, key2: value2}`
+	"""
+	
 	data = data.split("=")
 	data = data[len(data) - 1]
 	
